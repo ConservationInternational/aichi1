ee75a9dbe0e8a3429161a5ab75ae31f55b1d88cd
mcooper <mw.coop.r@gmail.com>
2017-10-05 14:42:12 +0000

    add time to output, error logging

--- scrape_wikipedia/scrape.R
+++ scrape_wikipedia/scrape.R
@@ -1,4 +1,4 @@
-f <- read.csv('species.csv', header=F)
+f <- read.csv('species.csv', header=F, stringsAsFactors=F)
 
 library(rvest)
 library(dplyr)
@@ -21,7 +21,7 @@ for (i in which(f$V1=='Stenoglottis inandensis'):length(f$V1)){
     write.csv(data.frame(species=f$V1[i], size=out, views=views$views), paste0('AllSpecies/', t, '.csv'), row.names=F)
   }
 
-  cat(paste0(round(i/nrow(f)*100, 4), '%'), f$V1[i], '\n')
+  cat(paste0(round(i/nrow(f)*100, 4), '%'), Sys.time(), f$V1[i], '\n')
 
-  }, error=function(e){})
+  }, error=function(e){cat(f$V1[1], '\n', file='errors.txt', append=T)})
 }
