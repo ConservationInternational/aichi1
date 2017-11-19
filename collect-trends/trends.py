cf7b02e2f36cf1e463803e3f5149dc3cb7d60d9e
mcooperci <mw.coop.r@gmail.com>
2017-11-19 15:19:09 -0500

    sample from google trends

---
+++ collect-trends/trends.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 12:12:58 2017
+
+@author: mcooper
+"""
+
+import os
+import pandas as pd
+from pytrends.request import TrendReq
+import datetime
+import time
+from pymongo import MongoClient
+
+data = pd.read_csv("../collect-twitter/issues.csv", encoding='utf-8')
+kw_list = data['google_topic_id'].tolist()
+keyword = data['en'].tolist()
+
+countries = pd.read_csv("../countries.csv")
+
+today = datetime.date.today()
+first = today.replace(day=1)
+lastMonth = first - datetime.timedelta(days=1)
+lastMonth.strftime("%Y%m")
+
+#Connect to MongoDB
+client = MongoClient('localhost', 27017)
+trendscon = client.TWITTER['TRENDS']
+
+today = datetime.date.today()
+#Get all data from previous month if the date is the first
+if today.day==1:
+    yesterday = today - datetime.timedelta(days=1)
+    begin = yesterday.replace(day=1).strftime("%Y-%m-%d")
+    end = yesterday.strftime("%Y-%m-%d")
+    month = yesterday.strftime("%Y-%m")
+    
+#Or get all data from the 1st of this month to today
+else:
+    begin = today.replace(day=1).strftime("%Y-%m-%d")
+    end = today.strftime("%Y-%m-%d")
+    month = today.strftime("%Y-%m")
+
+#temporarily get October 2017 data
+begin = '2017-10-01'
+end = '2017-10-31'
+month = '2017-10'
+#########################################
+
+timeframe = begin + ' ' + end
+
+pytrends = TrendReq(hl='en-US', tz=360)
+for i in range(0,len(kw_list)):
+    pytrends.build_payload([kw_list[0]], timeframe=timeframe, cat=0, geo='', gprop='')
+    out = pytrends.interest_by_region(resolution='COUNTRY')
+    
+    out['name'] = out.index
+      
+    #Check Google Trends country names match our country names
+    for n in out['name']:
+        if n not in countries['name'].tolist():
+            print('WARNING! ' + n + ' missing from countries table')
+    
+    comb = pd.merge(out, countries, how='inner', on='name')
+    
+    for j in range(0, len(comb['alpha-2'])):
+        incDict = {'country' : comb['alpha-2'][j], 'month' : month, 'issue': keyword[i]}
+        rate = comb[kw_list[i]][j]
+
+        post = trendscon.find_one(incDict)
+        if post is None:
+            incDict['rate'] = rate
+            trendscon.insert(incDict)
+        else:
+            post['rate'] = rate
+            trendscon.save(post)
+    
+    time.sleep(10)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
