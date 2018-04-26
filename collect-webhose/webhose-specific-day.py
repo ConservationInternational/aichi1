d97197829169224e92cee56c50075d3ed8beec9e
mcooper <mw.coop.r@gmail.com>
2018-04-26 01:24:22 +0000

    get article for specific date

--- collect-webhose/webhose-specific-day.py
+++ collect-webhose/webhose-specific-day.py
@@ -58,7 +58,7 @@ issues_melt = pd.melt(issues.drop('google_topic_id', axis=1))
 issues_melt = issues_melt.loc[issues_melt['value'] != 'XyEf9fAl2IuV6u97aM7a']
 
 
-for thetime in ['2018-01-13', '2018-01-14', '2018-01-15', '2018-01-16']:
+for thetime in ['2018-04-13', '2018-04-14', '2018-04-15', '2018-04-16', '2018-04-17', '2018-04-18', '2018-04-19']:
 ########################
 #Get times
 ########################
