21bbe984fa0dc48e8bde0c0b74560ae340350da5
Matthew Cooper <mw.coop.r@gmail.com>
2018-05-11 12:18:37 -0400

    search trends across study period by country

---
+++ collect-trends/trends-country.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pytrends.request import TrendReq
+import datetime
+import time
+import os
+
+os.chdir('/home/ec2-user/aichi1/collect-trends/')
+
+data = pd.read_csv("../collect-twitter/issues.csv", encoding='utf-8')
+kw_list = data['google_topic_id'].tolist()
+keyword = data['en'].tolist()
+
+countries = pd.read_csv("../countries.csv", encoding='utf-8', na_filter=False)
+countries.columns = ['country', 'ISO2']
+
+#Manually create timeframe like this:
+timeframe = '2017-11-01 2018-04-25'
+
+pytrends = TrendReq(hl='en-US', tz=360)
+
+all = pd.DataFrame({})
+for i in range(0,len(kw_list)):
+	pytrends.build_payload([kw_list[i]], timeframe=timeframe, cat=0, geo='', gprop='')
+	out = pytrends.interest_by_region(resolution='COUNTRY')
+	out['country'] = out.index
+	out['keyword'] = keyword[i]
+	out.columns = ['score', 'country', 'keyword']
+	all = pd.concat([all, out])
+
+all = pd.merge(all, countries)
+	
+all.to_csv('../myapp/public/csvs/DAILYTRENDS.csv', index=False)
