54942dbb4037fac95bcc3cab722953f026da5623
mcooper <mw.coop.r@gmail.com>
2018-11-05 20:46:58 +0000

    update trends-daily for Oct 2018

--- collect-trends/trends-daily.py
+++ collect-trends/trends-daily.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from pytrends.request import TrendReq
 
-data = pd.read_csv("issues.csv", encoding='utf-8')
+data = pd.read_csv("~/aichi1/collect-twitter/issues.csv", encoding='utf-8')
 kw_list = data['google_topic_id'].tolist()
 keyword = data['en'].tolist()
 
-countries = pd.read_csv("countries.csv", encoding='utf-8', na_filter=False)
+countries = pd.read_csv("~/aichi1/countries.csv", encoding='utf-8', na_filter=False)
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
 all = pd.DataFrame({})
 
 #Manually create timeframe like this:
-timeframe = '2017-11-01 2018-02-28'
+timeframe = '2017-11-01 2018-04-30'
 
 for i in range(0,len(kw_list)):
     pytrends.build_payload([kw_list[i]], timeframe=timeframe, cat=0, geo='', gprop='')
@@ -27,7 +27,7 @@ for i in range(0,len(kw_list)):
     all = pd.concat([all, out])
 
 #Manually create timeframe like this:
-timeframe = '2018-03-01 2018-07-31'
+timeframe = '2018-05-01 2018-10-31'
 
 for i in range(0,len(kw_list)):
     pytrends.build_payload([kw_list[i]], timeframe=timeframe, cat=0, geo='', gprop='')
@@ -42,3 +42,6 @@ for i in range(0,len(kw_list)):
     all = pd.concat([all, out])
 
 all.to_csv('DAILYTRENDS.csv', index=False)
+
+all.to_csv('~/aichi1/myapp/public/csvs/DAILYTRENDS.csv', index=False)
+
